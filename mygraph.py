f0a48a4fc6a244bfcb5489fa223aade93e1204cf
Giza <aranetagian4221@gmail.com>
2024-04-01 20:38:53 +0800

    okay

--- mygraph.py
+++ mygraph.py
@@ -1,20 +1,5 @@
-from numpy import genfromtxt
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 import networkx as nx
-
-mydata = genfromtxt('mycsv.csv', delimiter=',')
-print(mydata)
-print(type(mydata))
-adjacency = mydata[1:,1:]
-print(adjacency)
-
-def show_graph_with_labels(adjacency_matrix, mylabels):
-    rows, cols = np.where(adjacency_matrix == 1)
-    edges = zip(rows.tolist(), cols.tolist())
-    gr = nx.Graph()
-    gr.add_edges_from(edges)
-    nx.draw(gr, node_size=500, labels=mylabels, with_labels=True)
-    plt.show()
-
-show_graph_with_labels(adjacency, make_label_dict(get_labels('mycsv.csv')))
+input_data = pd.read_csv('mycsv.csv', index_col=0)
+G = nx.DiGraph(input_data.values)
+nx.draw(G)
