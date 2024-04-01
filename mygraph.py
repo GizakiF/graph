60c5674b75be8b7e39b021e1beac529a8a259292
Giza <aranetagian4221@gmail.com>
2024-04-01 21:04:24 +0800

    it works but not as intended (labels should be abcs)

--- mygraph.py
+++ mygraph.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
+
+# Load data
 input_data = pd.read_csv('mycsv.csv', index_col=0)
+
+# Create a directed graph
 G = nx.DiGraph(input_data.values)
-nx.draw(G)
+
+# Draw the graph
+pos = nx.spring_layout(G)  # you can also use other layout algorithms
+nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=1500, arrowsize=20)
+plt.show()
+
